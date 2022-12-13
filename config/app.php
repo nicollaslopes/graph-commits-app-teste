87ddd1d2bf355e47fe55c2b14e044c038f3f1fd5
nicollas.lopes <nicollaslopesn@gmail.com>
2022-12-13 20:01:17 -0300

    config5

--- config/app.php
+++ config/app.php
@@ -52,9 +52,9 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://graph-commits-app-hi9w.vercel.app/'),
+    'url' => env('APP_URL', 'https://graph-commits-app.vercel.app/'),
 
-    'asset_url' => env('ASSET_URL', 'https://graph-commits-app-hi9w.vercel.app/'),
+    'asset_url' => env('ASSET_URL', 'https://graph-commits-app.vercel.app/'),
 
     /*
     |--------------------------------------------------------------------------
