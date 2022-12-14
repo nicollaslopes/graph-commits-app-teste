15e924ca4f9f5e8f32210843cc96ae0cbb50d3e0
nicollas.lopes <nicollaslopesn@gmail.com>
2022-12-14 13:54:52 -0300

    alterando configs do app

--- config/app.php
+++ config/app.php
@@ -52,9 +52,9 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://localhost'),
+    'url' => env('APP_URL', 'http://localhost'),
 
-    'asset_url' => env('ASSET_URL', 'public'),
+    'asset_url' => env('ASSET_URL', null),
 
     /*
     |--------------------------------------------------------------------------
