c35eff822a6122057b4eef462a505068e5776406
nicollas.lopes <nicollaslopesn@gmail.com>
2022-12-14 09:42:11 -0300

    alterando de localhost para url publica

--- config/services.php
+++ config/services.php
@@ -33,6 +33,6 @@ return [
     'github' => [
         'client_id' => env('GITHUB_CLIENT_ID'),
         'client_secret' => env('GITHUB_CLIENT_SECRET'),
-        'redirect' => 'http://localhost:8000/auth/callback',
+        'redirect' => 'https://graph-commits-app.vercel.app/auth/callback',
     ],
 ];
